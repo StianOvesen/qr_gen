97a994f55033b83197a42337d0b5e7193f3a3dc4
StianOvesen <stianovesen@me.com>
2022-08-23 19:29:16 +0200

    Changed output from SVG to PNG

--- qrcode-gen.py
+++ qrcode-gen.py
@@ -2,8 +2,9 @@
 ### STIAN OVESEN
 
 import csv
-import qrcode.image.svg
+import qrcode.image.pil
 import os
+
 
 
 ### DELETE PREVIOUS QR-CODES
@@ -14,7 +15,8 @@ for file in os.listdir(dir):
 
 
 ### OPEN CSV AND GENERATE QR-CODES.
-factory = qrcode.image.svg.SvgPathImage
+factory = qrcode.image.pil.PilImage
+
 
 with open('objects.csv') as csvFile:
     csvReader = csv.reader(csvFile, delimiter=';')
@@ -22,6 +24,6 @@ with open('objects.csv') as csvFile:
         guid, number, filename = line
         # generate the QR code SVG using the url
         img = qrcode.make(guid, image_factory=factory)
-        img.save(f'codes/{number} {filename}.svg')
+        img.save(f'codes/{number} {filename}.png')
         print(f'{filename}.svg created with {guid} guid')
 
