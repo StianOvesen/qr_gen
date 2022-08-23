7b17be80e76d6209b59acf7e455b57f3e00bf9cc
StianOvesen <stianovesen@me.com>
2022-08-23 09:32:54 +0200

    file names now include object number

--- qrcode-gen.py
+++ qrcode-gen.py
@@ -1,3 +1,6 @@
+### WRITTEN BY:
+### STIAN OVESEN
+
 import csv
 import qrcode.image.svg
 import os
@@ -10,16 +13,15 @@ for file in os.listdir(dir):
     os.remove(os.path.join(dir, file))
 
 
-
 ### OPEN CSV AND GENERATE QR-CODES.
 factory = qrcode.image.svg.SvgPathImage
 
 with open('objects.csv') as csvFile:
     csvReader = csv.reader(csvFile, delimiter=';')
     for line in csvReader:
-        guid, filename = line
+        guid, number, filename = line
         # generate the QR code SVG using the url
         img = qrcode.make(guid, image_factory=factory)
-        img.save(f'codes/{filename}.svg')
+        img.save(f'codes/{number} {filename}.svg')
         print(f'{filename}.svg created with {guid} guid')
 
